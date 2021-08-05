19c9f304d4918b24999098ff18dafbfae9412aaf
lee <white11190709@gmail.com>
2021-08-05 11:32:49 +0800

    修改成 LF

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-# deploy.sh
 #!/usr/bin/env sh
 # 當發生錯誤時終止腳本運行
 set -e
@@ -6,7 +5,7 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/White12343/snacks-vue.git 分支為 gh-pages
